11e0f0b7fd32c1e3f44407c9149ab427b4d46324
raulovidiu <raulhuluban@gmail.com>
2015-01-05 23:08:00 +0200

    implemented first event

--- meteor_leaderboard.js
+++ meteor_leaderboard.js
@@ -1,16 +1,21 @@
 // Create a MongoDB Collection which works on the client and also on the server, like a "global variable"
-PlayerList = new Mongo.Collection('players');
+PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient) {
-
     // Helper functions execute code within templates
     Template.leaderboard.helpers({
         'player': function() {
             // Retrieve all of the data from the "PlayersList" collection
-            return PlayerList.find();
+            return PlayersList.find();
         }
     });
 
+    // Events trigger code when certain actions are taken
+    Template.leaderboard.events({
+        'click .player': function() {
+            console.log("clicked on a li from the .player class");
+        }
+    });
 };
 
 
